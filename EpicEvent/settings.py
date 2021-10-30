46288cc4386adef7a711de0ad9b44bc1dc3147b7
Fabienne RONDI <f.rondi@orange.fr>
2021-10-30 12:45:23 +0200

    database creation and settings update

--- EpicEvent/settings.py
+++ EpicEvent/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'EpicEvent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'epicevent',
+        'USER': 'epicadmin1',
+        'PASSWORD': 'cHirAt42*',
+        'HOST': 'localhost',
+        'PORT':'4532',
     }
 }
 

861c93ac2b564ddedf5ba2eadaca7cd9e7a23ac5
Fabienne RONDI <f.rondi@orange.fr>
2021-10-30 12:59:43 +0200

    sentry installation

--- EpicEvent/settings.py
+++ EpicEvent/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +134,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+sentry_sdk.init(
+    dsn="https://fd3186b5f94e456f8191db79dab4e9fa@o1055466.ingest.sentry.io/6041608",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
